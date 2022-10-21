57aa8cd8b5b3c38b642e74aae3ed65706a741944
Helga22-777 <91066438+Helga22-777@users.noreply.github.com>
2022-10-21 21:03:08 +0300

    Delete calculyatorJS(wokers) directory

--- calculyatorJS(wokers)/worker/worker1.js
+++
@@ -1,6 +0,0 @@
-onmessage = function (e) {
-    console.log('Получено сообщение от основного потока');
-    let workerResult = 'Результат: ' + (Number(e.data[0])  + Number(e.data[1]));
-    console.log('Отправка сообщения в основной поток');
-    postMessage(workerResult);
-}
