ca9ec80ae17132f0f77e3bcdeafa99a1ed51a216
Helga22-777 <91066438+Helga22-777@users.noreply.github.com>
2025-01-23 22:36:18 +0200

    Add files via upload

---
+++ visit_page/js/index.js
@@ -0,0 +1,77 @@
+let timerId;
+const nav = document.querySelector('nav')
+const iconMenu = document.querySelector('.menu-icon' )
+const iconMenuOpen = document.querySelector('.menu-icon-open')
+
+document.querySelectorAll(".dropdown-toggle").forEach(el => {
+ 
+  el.addEventListener('click' , (e) => {
+    const menu = e.target.dataset.path;
+
+    document.querySelectorAll(".dropdown-menu").forEach( item => {
+
+      item.classList.remove("menu-active");
+      item.classList.remove("open");
+
+      if(!document.querySelector(`[data-target=${menu}]`).classList.contains("open")) {
+        document.querySelector(`[data-target=${menu}]`).classList.add("menu-active")
+        timerId = setTimeout(() => {
+          document.querySelector(`[data-target=${menu}]`).classList.add("open")
+        }, 0)
+      }
+
+      if(document.querySelector(`[data-target=${menu}]`).classList.contains("open")) {
+        document.querySelector(`[data-target=${menu}]`).classList.remove("menu-active")
+        timerId = setTimeout(() => {
+          document.querySelector(`[data-target=${menu}]`).classList.remove("open")
+        }, 0)
+        
+      }
+      clearTimeout(timerId)
+
+      item.addEventListener('mouseleave', (e) => {
+        document.querySelector(`[data-target="${menu}"]`).classList.remove("menu-active");
+        document.querySelector(`[data-target="${menu}"]`).classList.remove("open");
+      })
+    })
+  })
+  
+})
+
+iconMenu.addEventListener('click', (e) => {
+  console.log('clikc');
+    nav.classList.add('active-side');
+    iconMenu.style.display = 'none';
+    iconMenuOpen.style.display = 'block';
+  
+})
+
+iconMenuOpen.addEventListener('click', (e) => {
+
+    nav.classList.remove('active-side');
+    iconMenu.style.display = 'block';
+    iconMenuOpen.style.display = 'none';
+
+})
+
+window.onclick = (e) => {
+  const target = e.target;
+  if (!target.closest('.dropdown-toggle') && !target.closest('.menu-icon') && !target.closest('nav')) {
+    nav.classList.remove('active-side');
+    iconMenu.style.display = 'block';
+    iconMenuOpen.style.display = 'none';
+  }
+};
+
+window.addEventListener('resize', (e) => {
+  const screenSize = window.innerWidth
+
+  if(screenSize > 772) {
+    nav.classList.remove('active-side');
+    iconMenu.style.display = 'none';
+    iconMenuOpen.style.display = 'none';
+  } else {
+    iconMenu.style.display = 'block';
+    iconMenuOpen.style.display = 'none';
+  }
+})
